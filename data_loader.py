2af05a45805f7c2d9efcedaaadff2433fd1ab43a
Sen Lin <rlinh99@outlook.com>
2022-03-20 15:08:32 +0800

    1
    1

--- data_loader.py
+++ data_loader.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import numpy as np
+from keras.preprocessing.text import Tokenizer
 
 
 def load_data(type):
     path = 'data/'
     if type == 'train':
-        path = path+'training.csv'
+        path = path + 'training.csv'
     if type == 'validation':
         path = path + 'validation.csv'
     return pd.read_csv(path)
 
 
+train_data = load_data('train')
+test_data = load_data('validation')
+print(len(train_data))
